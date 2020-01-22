5f499e5862c84083025d8d551b41a21abeb282e3
Shaneen Vitug <shaneenvitug@gmail.com>
2020-01-22 21:38:25 +1100

    added material ui library and input search field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <h1>Weather app coming soon!</h1>
-      <p>Search</p>
+       <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="City" />
+      </form>
     </div>
   );
 }
