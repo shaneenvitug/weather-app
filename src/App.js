77b89791bffcbd40e21993da50a9c52e02315dce
Shaneen Vitug <shaneenvitug@gmail.com>
2020-01-25 13:18:59 +1100

    initial integration of weather api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Fab from '@material-ui/core/Fab';
@@ -14,15 +15,27 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function App() {
+  const [city, setCity] = useState();
+
   const classes = useStyles();
+
+  const getWeather = () => {
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=dc0ba9ecf138c1e8167017659deef39b`).then(res => {
+      console.log(res);
+    })
+  }
+
+  const handleChange = e => {
+    setCity(e.target.value);
+  }
 
   return (
     <div className="App">
       <h1>Weather app coming soon!</h1>
        <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="City" />
+        <TextField id="standard-basic" label="City" value={city} onChange={handleChange}/>
         {/* button material ui */}
-        <Fab variant="extended">
+        <Fab variant="extended" onClick={() => getWeather()} >
           Search
         </Fab>
       </form>
