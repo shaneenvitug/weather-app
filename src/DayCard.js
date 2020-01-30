45c66926dbac0543da4230d04c2870ed1d20910a
Shaneen Vitug <shaneenvitug@gmail.com>
2020-01-30 16:33:43 +1100

    added day card material ui style

--- src/DayCard.js
+++ src/DayCard.js
@@ -1,18 +1,42 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import './DayCard.css';
 var moment = require('moment');
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 50,
+    margin: 30,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const DayCard = ({reading}) => {
   let newDate = new Date();
   const weekday = reading.dt * 1000
   newDate.setTime(weekday)
 
+  const classes = useStyles();
+
   return (
-    <div className="dayCard">
-      <img className="icon" src={`http://openweathermap.org/img/wn/${reading.weather[0].icon}@2x.png`} alt={reading.weather[0].icon}/>
-      <p className="dayTemp">{Math.round(reading.main.temp)}&deg;</p>
-      <p>{moment(newDate).format('ddd')}</p>
-    </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          <img className="icon" src={`http://openweathermap.org/img/wn/${reading.weather[0].icon}@2x.png`} alt={reading.weather[0].icon}/>
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          <p className="dayTemp">{Math.round(reading.main.temp)}&deg;</p>
+        </Typography>
+        <Typography variant="body2" component="p">
+          <p>{moment(newDate).format('ddd')}</p>
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
